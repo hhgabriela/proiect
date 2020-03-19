a52da6b6969214fb92260c135ee26f77ab747ec7
Gabriela Zarnescu <zarnescu_gabriela@yahoo.com>
2020-03-19 22:10:40 +0200

    added src

---
+++ lab1-master/src/loose/oose/fis/processors/Procesor.java
@@ -0,0 +1,10 @@
+package loose.oose.fis.processors;
+
+import loose.oose.fis.documents.Document;
+
+import java.util.ArrayList;
+
+public interface Procesor {
+    int proceseaza(ArrayList<Document> documente);
+    int i;
+}
